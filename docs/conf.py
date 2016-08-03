4109ac6943b1d74570360676a17a35b7a7c40b4b
Christopher Trudeau <ctrudeau@arsensa.com>
2016-08-03 14:59:39 -0400

    Update copyright date in docs

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'DForm'
-copyright = '2015, Christopher Trudeau'
+copyright = '2015-2016, Christopher Trudeau'
 author = 'Christopher Trudeau'
 
 # The version info for the project you're documenting, acts as replacement for
