7f359fef5791aa8615c4298261c28e584f791c17
Christopher Trudeau <ctrudeau@arsensa.com>
2016-08-04 13:10:32 -0400

    Fix missing ^ in url patterns

--- dform/tests/urls.py
+++ dform/tests/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
-    url(r'rankedmodel/', include('awl.rankedmodel.urls')),
+    url(r'^rankedmodel/', include('awl.rankedmodel.urls')),
 
-    url(r'dform/', include('dform.urls')),
-    url(r'dform_admin/', include('dform.admin_urls')),
+    url(r'^dform/', include('dform.urls')),
+    url(r'^dform_admin/', include('dform.admin_urls')),
 ]
